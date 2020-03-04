f51380396c547512896a1e8e65f82c64943d0244
n8programs <43304488+N8python@users.noreply.github.com>
2020-03-03 21:11:58 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,64 @@
+new Vivus('about', {
+    duration: 500
+});
+new Vivus("projects", {
+    duration: 500
+})
+new Vivus("blog", {
+    duration: 500
+})
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const blog = document.getElementById("blog");
+const main = document.getElementById("main");
+let mode = "none";
+about.onclick = async() => {
+    if (mode !== "about") {
+        mode = "about";
+        const html = await fetch("about.html");
+        const text = await html.text();
+        main.classList.remove("w3-animate-left");
+        setTimeout(() => {
+            main.innerHTML = text;
+            main.classList.add("w3-animate-left")
+        }, 100)
+    }
+}
+projects.onclick = async() => {
+    if (mode !== "projects") {
+        mode = "projects";
+        const html = await fetch("projects.html");
+        const text = await html.text();
+        main.classList.remove("w3-animate-left")
+        setTimeout(() => {
+            main.innerHTML = text;
+            main.classList.add("w3-animate-left")
+        }, 100)
+    }
+}
+const posts = [
+    ["Hello World", "March 3, 2020"]
+];
+const idify = string => string.toLowerCase().replace(/\s/g, "-");
+blog.onclick = () => {
+    if (mode !== "blog") {
+        mode = "blog";
+        main.classList.remove("w3-animate-left")
+        setTimeout(() => {
+            main.innerHTML = posts.map(([name, date]) => `<p id="${idify(name)}" style="cursor:pointer" class="w3-text-white">${name} - ${date}</p>`).join("\n");
+            main.classList.add("w3-animate-left")
+            posts.map(([name]) => idify(name)).forEach(id => {
+                document.getElementById(id).onclick = async() => {
+                    mode = id;
+                    const html = await fetch(`${id}.html`);
+                    const text = await html.text();
+                    main.classList.remove("w3-animate-left")
+                    setTimeout(() => {
+                        main.innerHTML = text;
+                        main.classList.add("w3-animate-left")
+                    }, 100)
+                }
+            })
+        }, 100);
+    }
+}
